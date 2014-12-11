ffaa2883605539c41bf2a3b9cd71693bbf29d9fe
bakasajoshua <baksajoshua09@gmail.com>
2014-12-11 16:25:17 +0300

    MY_Controller

---
+++ application/modules/home/controllers/home.php
@@ -0,0 +1,20 @@
+<?php if (!defined("BASEPATH")) exit("No direct access to the script is allowed");
+
+/**
+* 
+*/
+class Home extends MY_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		
+	}
+}
+
+?>
